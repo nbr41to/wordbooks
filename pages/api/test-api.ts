a814f654fff0136f7e6d178e5671f2d1cbfe10e9
nbr41to <nbr.41to@gmail.com>
2021-02-26 20:03:45 +0900

    .

--- pages/api/test-api.ts
+++ pages/api/test-api.ts
@@ -1,11 +1,11 @@
 // APIで遊ぶ
-import { firebase } from '../../src/firebase'
+import { firebase } from '../../src/firebase';
 
 export default async (req, res) => {
-  const text = await firebase.firestore().collection('test_db').doc('testes').get().then(doc => doc.data())
+  const text = await firebase.firestore().collection('test_db').doc('testes').get().then(doc => doc.data());
   // const snapshotText = await firebase.firestore().collection('test_db').doc('testes').onSnapshot(snapshot => snapshot.data())
   if (req.method === 'POST') {
-    res.status(200).json(text)
+    res.status(200).json(text);
     // res.status(200).json(snapshotText) // snapshotはエラー
   }
-}
+};
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,20 +1,29 @@
-import Link from 'next/link'
+import Link from 'next/link';
 import { Login } from '../src/components/Login';
 import { ModalWrapper } from '../src/components/Modal/Modal';
 import React from 'react';
 import styled from 'styled-components';
 import { Button } from '../src/components/atoms/Button';
 
-export default function Home() {
-  const [open, set] = React.useState(false)
-  const get = async () => {
+const Home: React.FC = () => {
+  const [open, set] = React.useState(false);
+  const get1 = async () => {
     const response = await fetch('http://localhost:3000/api/test-api', {
       method: 'post'
-    })
-    const data = await response.json()
-    console.log(data)
-  }
-  get()
+    });
+    const data = await response.json();
+    console.log(data);
+  };
+  get1();
+
+  const get2 = async () => {
+    // await fetch('https://jsonplaceholder.typicode.com/posts')
+    // await fetch('https://jsonplaceholder.typicode.com/user')
+    await fetch('https://jsonplaceholder.typicode.com/comments')
+      .then((response) => response.json())
+      .then((json) => console.log(json));
+  };
+  get2();
 
   return (
     <div>
@@ -30,10 +39,11 @@ export default function Home() {
         </Box>
       </ModalWrapper>
     </div>
-  )
-}
+  );
+};
+export default Home;
 
 const Box = styled.div`
   background-color: #fff;
   width: 200px;
-`
+`;
